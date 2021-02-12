4fda224912f6ba15929edcbbacf85adced1e5941
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-11 20:00:13 -0600

    routing implemented

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CoverComponent } from './cover/cover.component';
@@ -13,7 +15,8 @@ import { CoverComponent } from './cover/cover.component';
     CoverComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidebarComponent,
