d3135437d49bc6879541c1fa443dccd4f2b0a7dc
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-11 23:56:10 -0600

    register component created

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,12 +6,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 
 
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
