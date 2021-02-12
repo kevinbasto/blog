7a79c682d5ad54f7eeac3f609fdfe197dd514ab9
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-12 13:02:10 -0600

    basic form Sketching

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService
+  ) { }
+
+  public registerForm : FormGroup = this.fb.group({
+    username: ["", [Validators.required]],
+    email : ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    verifyPassword: ["", [Validators.required, Validators.minLength(6)]],
+    terms: ["", [Validators.required]]
+  });
 
   ngOnInit(): void {
+
+  }
+
+  register(){
+    console.log(this.registerForm.value);
   }
 
 }
