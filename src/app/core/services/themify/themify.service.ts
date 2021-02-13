8a4fb425c302e9881ea2b5694251810c1e497c3e
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-12 23:29:58 -0600

    themifying implemented

--- src/app/core/services/themify/themify.service.ts
+++ src/app/core/services/themify/themify.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+const themeMap : { [key : string] : string } = {
+  dark : "light",
+  light: "beige",
+  beige: "dark"
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemifyService {
 
-  constructor() { }
+  // public current : Observable<string>;
+  public theme : string;
+  public bodyClass : any;
+  
+
+  constructor() {
+    this.getCurrentTheme();
+   }
+
+  getCurrentTheme(){
+    this.theme = localStorage.getItem("theme")!;
+
+    if(!this.theme){
+      this.theme = Object.keys(themeMap)[0];
+      localStorage.setItem("theme", this.theme);
+    }
+    
+    this.bodyClass = document.body.classList;
+    this.bodyClass.add(this.theme);
+  }
+
+  changeTheme(){
+    let current : string = this.theme!;
+    let next = themeMap[current];
+    this.bodyClass.replace(current, next);
+    this.theme = next;
+    localStorage.setItem('theme', next); 
+  }
+  
 }
