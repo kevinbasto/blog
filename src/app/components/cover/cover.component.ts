3cc97d48aa299f006453a776468939ff937691e0
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-13 17:19:13 -0600

    cover styling and change

--- src/app/components/cover/cover.component.ts
+++ src/app/components/cover/cover.component.ts
@@ -1,4 +1,9 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router'
+import { switchMap } from 'rxjs/operators';
+import { CoverService } from '../../core/services/cover/cover.service';
+
 
 @Component({
   selector: 'app-cover',
@@ -8,18 +13,19 @@ import { Component, OnInit } from '@angular/core';
 export class CoverComponent implements OnInit {
 
   constructor(
-    
+    private cs : CoverService,
+    private router: Router
   ) { }
 
   public coverUrl : string = "https://cdn.wallpapersafari.com/7/69/dxBmov.jpg";
-  public title: string;
+  public title: string = "Inicio";
 
   ngOnInit(): void {
-    this.setTitle();
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+        console.log(this.cs.getTitle(event.url));
+        this.title = this.cs.getTitle(event.url);
+      }
+    });
   }
-
-  setTitle(){
-
-  }
-
 }
--- src/app/core/client.menu.ts
+++ src/app/core/client.menu.ts
@@ -3,25 +3,25 @@ import { MenuOption } from './menu.option';
 export const ClientMenu : Array<MenuOption> = [
     {
         title: "Inicio",
-        url : "client/inicio",
+        url : "/client/inicio",
         icon: "fa-newspaper",
         level: 1
       },
       {
         title: "Coreanas",
-        url :"client/coreanas",
+        url :"/client/coreanas",
         icon: "fa-yin-yang",
         level: 1
       },
       {
         title: "Originales",
-        url: "client/originales",
+        url: "/client/originales",
         icon: "fa-dragon",
         level: 1
       },
       {
         title: "Chinas",
-        url : "client/chinas",
+        url : "/client/chinas",
         icon: "fa-vihara",
         level: 1
       },
@@ -33,37 +33,37 @@ export const ClientMenu : Array<MenuOption> = [
       },
       {
         title: "+15/+19",
-        url: "client/mayores",
+        url: "/client/mayores",
         icon: "fa-surprise",
         level: 1
       },
       {
         title: "Terminadas",
-        url: "client/terminadas",
+        url: "/client/terminadas",
         icon: "fa-hourglass-end",
         level: 1
       },
       {
         title: "Boys love",
-        url: "client/boys-love",
+        url: "/client/boys-love",
         icon: "fa-heart",
         level: 1
       },
       {
         title: "Mi perfil",
-        url: "client/profile",
+        url: "/client/perfil",
         icon: "fa-user",
         level: 1
       },
       {
         title: "Sube tus proyectos",
-        url: "client/sube",
+        url: "/client/sube",
         icon: "fa-arrow-circle-up",
         level: 1
       },
       {
         title: "Ir a la consola de administrador",
-        url: "admin/inicio",
+        url: "/admin/inicio",
         icon: "fa-cogs",
         level: 1
       }
