c4672d90a058cbf39bb75b528ea7e96749c9c6a7
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-13 17:18:58 -0600

    profile routing

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
     path: 'inicio',
     component: InicioComponent
+  },
+  {
+    path: 'perfil',
+    component: ProfileComponent
   }
 ]
 
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 
 import { InicioComponent } from './inicio/inicio.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     InicioComponent,
+    ProfileComponent,
     
   ],
   imports: [
