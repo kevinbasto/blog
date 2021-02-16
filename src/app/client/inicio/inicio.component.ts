1487d0d40db8fbf4f0329f7612241a3a06bbcafe
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-16 00:34:18 -0600

    changes in inicio

--- src/app/client/inicio/inicio.component.ts
+++ src/app/client/inicio/inicio.component.ts
@@ -17,28 +17,28 @@ export class InicioComponent implements OnInit {
       url: "client/novelas/mhThkaJhDuQ348HtMB5v/5"
     },
     {
-      title: "Kobayashi-san chi no maid dragon",
+      title: "Tensei no shitara slime datta ken",
       description: "lorem ipsum dolor sit amet, consectetur adipisicing elit",
-      cover: "https://m.media-amazon.com/images/I/51qv2IdWLzL.jpg",
-      url: "client/novelas/mhThkaJhDuQ348HtMB5v/5"
+      cover: "https://images-na.ssl-images-amazon.com/images/I/81ajhj8RqML._AC_SY879_.jpg",
+      url: "client/novelas/mhThkaJhDuQ348HtMB5v/4"
+    },
+    {
+      title: "Dragon Ball Z",
+      description: "lorem ipsum dolor sit amet, consectetur adipisicing elit",
+      cover: "https://m.media-amazon.com/images/I/61nY1Y2nonL.jpg",
+      url: "client/novelas/mhThkaJhDuQ348HtMB5v/3"
+    },
+    {
+      title: "Otome Game no Hametsu Flag shika nai Akuyaku Reijou ni Tensei shite shimatta",
+      description: "lorem ipsum dolor sit amet, consectetur adipisicing elit",
+      cover: "https://otakuteca.com/images/books/cover/5cb938a29c423.jpg",
+      url: "client/novelas/mhThkaJhDuQ348HtMB5v/2"
     },
     {
       title: "Kobayashi-san chi no maid dragon",
       description: "lorem ipsum dolor sit amet, consectetur adipisicing elit",
       cover: "https://m.media-amazon.com/images/I/51qv2IdWLzL.jpg",
-      url: "client/novelas/mhThkaJhDuQ348HtMB5v/5"
-    },
-    {
-      title: "Kobayashi-san chi no maid dragon",
-      description: "lorem ipsum dolor sit amet, consectetur adipisicing elit",
-      cover: "https://m.media-amazon.com/images/I/51qv2IdWLzL.jpg",
-      url: "client/novelas/mhThkaJhDuQ348HtMB5v/5"
-    },
-    {
-      title: "Kobayashi-san chi no maid dragon",
-      description: "lorem ipsum dolor sit amet, consectetur adipisicing elit",
-      cover: "https://m.media-amazon.com/images/I/51qv2IdWLzL.jpg",
-      url: "client/novelas/mhThkaJhDuQ348HtMB5v/5"
+      url: "client/novelas/mhThkaJhDuQ348HtMB5v/1"
     }
   ]
 
