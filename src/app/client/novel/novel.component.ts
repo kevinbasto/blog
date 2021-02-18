9243f21d5dd33e6c3a72c275ee984e33627bc788
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-18 17:14:41 -0600

    novel sketching and design complete

--- src/app/client/novel/novel.component.ts
+++ src/app/client/novel/novel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Novel } from 'src/app/core/interfaces/novel';
+import { NovelService } from 'src/app/core/services/novel/novel.service';
+
 
 @Component({
   selector: 'app-novel',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ns : NovelService
+  ) { }
+
+  public novel : Novel
+  public chapters : Array<any>;
 
   ngOnInit(): void {
+    this.getData();
   }
 
+  getData(){
+    this.ns.getNovelData()
+    .then((novel : Novel) => {
+      this.novel = novel;
+      this.setChaptersBox();
+    })
+    .catch(error => console.error(error));
+  }
+
+  setChaptersBox(){
+    this.chapters = [];
+    for(let i = 1; i <= this.novel.chapters; i++){
+      this.chapters.push({
+        number: i,
+        url: `/client/${this.ns.genre}/${this.ns.id}/${i}`
+      });
+    }
+    
+  }
 }
