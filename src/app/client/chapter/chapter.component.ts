f88efbf3b089a866857979a8f25b8002e13b79c8
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-21 17:18:21 -0600

    chapter component

--- src/app/client/chapter/chapter.component.ts
+++ src/app/client/chapter/chapter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChapterService } from 'src/app/core/services/chapter/chapter.service';
+import { Chapter } from 'src/app/core/interfaces/chapter';
 
 @Component({
   selector: 'app-chapter',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChapterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private cs : ChapterService
+  ) { }
+
+  public chapter : Chapter;
 
   ngOnInit(): void {
+    this.getChapter();
   }
 
+  getChapter(){
+    this.cs.getChapter()
+    .then((chapter : Chapter) => {
+      this.chapter = chapter;
+      console.log(chapter);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
 }
--- src/app/core/services/chapter/chapter.service.ts
+++ src/app/core/services/chapter/chapter.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { NavigationEnd, Router } from '@angular/router';
+import { Chapter } from '../../interfaces/chapter';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChapterService {
 
-  constructor() { }
+  constructor(
+    private af : AngularFirestore,
+    private router: Router
+  ) { 
+    this.getRouteNodes();
+  }
+
+  public genre : string;
+  public novel : string;
+  public chapter: string;
+
+  getRouteNodes(){
+    let nodes : Array<string>;
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+        nodes = event.url.split("/").filter(node => {
+          if(node != "" && node != "client"){
+            return node;
+          }
+        })
+
+        this.genre = nodes[0];
+        this.novel = nodes[1];
+        this.chapter = nodes[2];
+      }
+    });
+  }
+
+  getChapter(){
+     return new Promise<any>((resolve, reject) => {
+       this.af
+       .collection(this.genre)
+       .doc(this.novel)
+       .collection("chapters")
+       .doc(this.chapter)
+       .valueChanges()
+       .subscribe(chapter => {  
+         if(chapter)
+          resolve(chapter);
+         else
+          reject("lo sentimos, no existe el capítulo :(");
+       })
+     });
+  }
+
+
 }
