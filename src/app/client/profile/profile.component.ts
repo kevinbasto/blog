3518366500b4ee07b72179939c872218fdeb7e80
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-22 11:06:41 -0600

    getting user info

--- src/app/client/profile/profile.component.ts
+++ src/app/client/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProfileService } from 'src/app/core/services/profile/profile.service';
 
 
 @Component({
@@ -10,16 +11,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ProfileComponent implements OnInit {
 
   constructor(
-    private fb : FormBuilder
+    private fb : FormBuilder,
+    private ps : ProfileService
   ) { }
 
   public profileForm : FormGroup = this.fb.group({
     username : ["", [Validators.required]],
     email : ["", [Validators.required]]
   });
+  public error: string;
 
   ngOnInit(): void {
     this.profileForm.controls["username"].disable();
     this.profileForm.controls["email"].disable();
+    this.ps.getProfile();
+  }
+
+  getProfile(){
+    this.ps.getProfile()
+    .then(user => {
+      
+    })
+    .catch(error => {
+      this.error = error;
+    });
   }
 }
--- src/app/core/services/profile/profile.service.ts
+++ src/app/core/services/profile/profile.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(
+    private af: AngularFirestore,
+    private auth: AuthService
+  ) { }
+
+  getProfile(){
+    this.auth.user$.subscribe(user => {
+      console.log(user);
+    })
+  }
 }
