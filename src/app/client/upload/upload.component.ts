3df8dbc772a3d3a6ef2396c548b7b08bc929afeb
Kevin Basto <neverknowpeople@hotmail.com>
2021-02-23 20:22:55 -0600

    upload form request implemented

--- src/app/client/upload/upload.component.ts
+++ src/app/client/upload/upload.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UploadService } from 'src/app/core/services/upload/upload.service';
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb : FormBuilder,
+    private us: UploadService
+  ) { }
+
+  public uploadForm : FormGroup = this.fb.group({
+    name : ["", [Validators.required]],
+    reason : ["", [Validators.required]],
+    genres : ["", [Validators.required]],
+    terms : [null, [Validators.required]]
+  });
+  public message : string;
+  public uploading : boolean = false;
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    this.uploading = !this.uploading;
+    let fields = ["name", "reason", "genres"];
+    let form : any = {};
+
+    for(let field of fields){
+      form[field] = this.uploadForm.get(field).value;
+    }
+
+    this.us.submit(form.name, form.reason, form.genres)
+    .then(res => {
+      this.message = "Hemos recibido tu solicitud, ¡Nos comunicaremos contigo apenas podamos!";
+      this.uploading = false;
+    })
+    .catch(error => {
+      this.message = "Hubo un problema a la hora de subir tu solicitud :(";
+    })
   }
 
 }
