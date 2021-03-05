8cfc37c6d437a508cd8936e8bde1aef0bbfd7041
Kevin Basto <neverknowpeople@hotmail.com>
2021-03-05 11:25:23 -0600

    now table has support for landing page in admin

--- src/app/core/services/table/table.service.ts
+++ src/app/core/services/table/table.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
-import { novelTitles } from '../../models/novel.model';
+import { novelModel, novelTitles } from '../../models/novel.model';
 import { requestModel, requestTitles } from '../../models/request.model';
 import { staffModel, staffTitles } from '../../models/staff.model';
+import { updatesModel, updatesTitles } from '../../models/updates.model';
 import { userModel, userTitles } from '../../models/user.model';
 
 @Injectable({
@@ -46,7 +47,7 @@ export class TableService {
   setHeaders(table : string){
     switch(table){
       case "inicio":
-        
+        this.headers = updatesTitles;
       break;
       case "usuarios":
         this.headers = userTitles;
@@ -66,7 +67,7 @@ export class TableService {
   setModel(table : string){
     switch(table){
       case "inicio":
-
+        this.model = updatesModel;
       break;
       case "usuarios":
         this.model = userModel;
@@ -78,7 +79,7 @@ export class TableService {
         this.model = staffModel;
       break;
       default:
-
+        this.model = novelModel
       break;
     } 
   }
