f5ec3f4a21a158a9cd1ba20b8b571ad876b4def1
Kevin Basto <neverknowpeople@hotmail.com>
2021-03-07 14:31:13 -0600

    set table name

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TableService } from 'src/app/core/services/table/table.service';
 
 @Component({
   selector: 'app-table',
@@ -7,12 +8,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private table : TableService
+  ) { }
 
   @Input() collection : string;
 
   ngOnInit(): void {
   }
-
-
 }
--- src/app/core/services/table/table.service.ts
+++ src/app/core/services/table/table.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TableService {
 
-  constructor() { }
+  constructor(private ds : DataService) { }
+
+  public collection : string;
+
+  // this sets the source of the data for the data service
+  setCollection(collection : string){
+    this.collection = collection;
+  }
+
 }
