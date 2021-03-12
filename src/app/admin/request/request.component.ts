344ac71f8a0fa73ba0bae9312ade474b77c47418
Kevin Basto <neverknowpeople@hotmail.com>
2021-03-11 23:19:51 -0600

    minor sketching in the app

--- src/app/admin/request/request.component.ts
+++ src/app/admin/request/request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-request',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+    this.getRequestid();
+  }
+
+  getRequestid() {
+    let request = this.router.url.substr(1, this.router.url.length - 1);
+    request = request.split("/")[request.split("/").length - 1];
+    
   }
 
 }
