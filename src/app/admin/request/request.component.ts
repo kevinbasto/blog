bde0aa6e48e58a7b10728517ce672c7d87d35828
Kevin Basto <neverknowpeople@hotmail.com>
2021-03-11 23:52:29 -0600

    request form implemented

--- src/app/admin/request/request.component.ts
+++ src/app/admin/request/request.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { RequestService } from 'src/app/core/services/request/request.service';
 
 @Component({
   selector: 'app-request',
@@ -9,8 +11,20 @@ import { Router } from '@angular/router';
 export class RequestComponent implements OnInit {
 
   constructor(
-    private router : Router
+    private router : Router,
+    private rs : RequestService,
+    private fb : FormBuilder
   ) { }
+
+  public id : string
+  public request  : any;
+  public requestForm : FormGroup = this.fb.group({
+    date : [""],
+    email : [""],
+    genres: [""],
+    name: [""],
+    reason : [""]
+  });
 
   ngOnInit(): void {
     this.getRequestid();
@@ -19,7 +33,19 @@ export class RequestComponent implements OnInit {
   getRequestid() {
     let request = this.router.url.substr(1, this.router.url.length - 1);
     request = request.split("/")[request.split("/").length - 1];
-    
+    this.id = request;
+    this.getRequest();
+  }
+
+  getRequest() {
+    this.rs.getRequest(this.id)
+    .then(request => {
+      console.log(request);
+      this.request = request;
+    })
+    .catch(error => {
+      console.log(error);
+    })
   }
 
 }
--- src/app/core/services/request/request.service.ts
+++ src/app/core/services/request/request.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor() { }
+  constructor(
+    private af : AngularFirestore
+  ) { }
+
+  getRequest(id : string){
+    return new Promise<any>(async (resolve, reject) => {
+      await this.af.doc(`/requests/${id}`)
+      .valueChanges()
+      .pipe(
+        take(1),
+      )
+      .toPromise()
+      .then(request => resolve(request))
+      .catch(error => reject(error));
+    })
+  }
 }
