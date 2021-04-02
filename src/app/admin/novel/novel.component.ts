76e7e838e63fcc9af3d8204d095f44efc156dfef
Kevin Basto <neverknowpeople@hotmail.com>
2021-04-01 18:38:42 -0600

    functional sketch of the novelform

--- src/app/admin/novel/novel.component.ts
+++ src/app/admin/novel/novel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NovelService } from 'src/app/core/services/novel/novel.service';
+import { StaffService } from 'src/app/core/services/staff/staff.service';
 
 @Component({
   selector: 'app-novel',
@@ -11,75 +12,54 @@ import { NovelService } from 'src/app/core/services/novel/novel.service';
 export class NovelComponent implements OnInit {
 
   constructor(
-    private router : Router,
-    private ns : NovelService,
-    private fb : FormBuilder
-  ) { }
+    private router: Router,
+    private novelService : NovelService,
+    private fb : FormBuilder,
+    private staffService : StaffService
+  ) {
+    
+  }
 
-  public novel : string;
-  public content : any;
-  public novelForm : FormGroup = this.fb.group({
-    title : [""],
-    description: [""],
-    translators : this.fb.array([]),
-    finished: [null]
-  })
+  public novelForm : FormGroup;
 
   ngOnInit(): void {
-    this.getRoute();
-  }
-
-  getRoute(){
-    let genre = this.router.url.split("/")[this.router.url.split("/").length - 2];
-    let novel = this.router.url.split("/")[this.router.url.split("/").length - 1];
-    if(novel == "new")
-      this.createNovel();
-    else
-      this.getNovel(genre, novel);
-  }
-
-  createNovel(){
-    this.novel = "Crear Novela";
-  }
-
-  getNovel(genre : string, novel : string){
-    this.ns.getNovel(genre, novel)
-    .then(novel => {
-      this.content = novel;
-      console.log(this.content);
-      this.novel = this.content.title;
-      this.setData();
+    this.novelForm = this.fb.group({
+      title: [""],
+      description: [""],
+      author: [""],
+      translators: this.fb.array([])
     })
-    .catch(error => {
-      console.log(error);
-    });
+
+    this.addTranslator();
   }
 
-  setData(){
-    let controls = ["title", "description"];
-    for(let control of controls){
-      this.novelForm.controls[control].setValue(this.content[control]);
-    }
+  addTranslator(){
+    let translator : FormGroup = this.fb.group({
+      uid: [""]
+    })
 
-    for(let translator of this.content.translators){
-      this.addTranslator(translator.uid, translator.username);
-      
-    }
+    this.translators.push(translator);
   }
 
-  get translatorForms(){
+  removeTranlator(i : number) {
+    this.translators.removeAt(i);
+  }
+
+  // getters to ease the use of the form
+  get title(){
+    return this.novelForm.get('title');
+  }
+
+  get description(){
+    return this.novelForm.get('description');
+  }
+
+  get Author(){
+    return this.novelForm.get('author');
+  }
+
+  get translators(){
     return this.novelForm.get('translators') as FormArray;
   }
 
-  addTranslator(uid : string, username : string){
-    let translator = this.fb.group({
-      uid: [null, Validators.required]
-    })
-
-    this.translatorForms.push(translator);
-  }
-
-  deleteTranslator(i : number){
-    this.translatorForms.removeAt(i);
-  }
 }
