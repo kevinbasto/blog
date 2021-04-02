d49f38b2da0d21e743a1d3f112023e2a638f9f0d
Kevin Basto <neverknowpeople@hotmail.com>
2021-04-01 23:06:21 -0600

    changes in code and sketch initialized

--- src/app/admin/chapter/chapter.component.ts
+++ src/app/admin/chapter/chapter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chapter',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChapterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  public genre : string;
+  public novel : string;
+  public chapter : string;
 
   ngOnInit(): void {
+    this.getUrlTree();
   }
 
+  getUrlTree() {
+    this.genre   = this.url.split("/")[this.url.split("/").length - 3];
+    this.novel   = this.url.split("/")[this.url.split("/").length - 2];
+    this.chapter = this.url.split("/")[this.url.split("/").length - 1];
+  }
+
+  get url(){
+    return this.router.url;
+  }
 }
